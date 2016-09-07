b55ce501c4bcd50d56b95be11d0923d053848aa9
Cory Tohill <corytohill@gmail.com>
2016-09-07 15:49:33 -0500

    added list views for Artist, Album, and Track

--- music_history/music_history_app/views.py
+++ music_history/music_history_app/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework import generics
+from music_history_app.models import Artist, Album, Track
+from music_history_app.serializers import ArtistSerializer, AlbumSerializer, TrackSerializer
 
-# Create your views here.
+
+class ArtistList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
+    queryset = Artist.objects.all()
+    serializer_class = ArtistSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class AlbumList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
+    queryset = Album.objects.all()
+    serializer_class = AlbumSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class TrackList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
+    queryset = Track.objects.all()
+    serializer_class = TrackSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
