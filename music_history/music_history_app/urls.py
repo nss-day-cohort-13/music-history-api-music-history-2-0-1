9fd2b5512fa557ea28b2c2c4f69f56109e0f08f2
Cory Tohill <corytohill@gmail.com>
2016-09-07 15:52:10 -0500

    added urls.py file to app

---
+++ music_history/music_history_app/urls.py
@@ -0,0 +1,13 @@
+from rest_framework import routers
+from django.conf.urls import url, include
+from music_history_app import views
+
+
+router = routers.DefaultRouter()
+router.register(r'aritsts', views.ArtistList)
+router.register(r'albums', views.AlbumList)
+router.register(r'tracks', views.TrackList)
+
+urlpatterns = [
+    url(r'^', include(router.urls)),
+]
